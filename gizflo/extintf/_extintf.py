0aec258c85f0fe358dfccd664e8b45a46a248842
Christopher Felton <chris.felton@gmail.com>
2014-09-30 19:35:27 -0500

    added external interfaces

---
+++ gizflo/extintf/_extintf.py
@@ -0,0 +1,19 @@
+
+class _extintf(object):
+    """ base class for external interface definitions.
+
+    An external interface is like a mini-board definition, it is
+    a collection of port definitions and associated attributes. 
+    The external intefaces are configured with higher-level 
+    definitions that are applicate to the interface.
+
+    An external interface needs the ports and pins.
+    """
+    
+    def __init__(self):
+        """ 
+        """
+        pass
+    
+
+    
---
+++ gizflo/extintf/_port.py
@@ -0,0 +1,32 @@
+
+import myhdl
+
+class _port(object):
+    """ generic port definitions 
+    A port is a "signal" in a top-level HDL module.  The ports
+    are mapped to pins on physical devices.  This object is used
+    to encapsulate all the attributes of an FPGA pin and associate
+    it with a port.
+    """
+
+    def __init__(self, name, pins, **pattr):
+        """ relate a port to a device pins and pin attributes"""
+        self.name = name
+        self.pins = pins
+        self.inuse = False
+        if len(pins) == 1:
+            self.sig = myhdl.Signal(bool(0))
+        else:
+            self.sig = myhdl.Signal(intbv(0)[len(pins):])
+
+        # device specific arguments, when the pin assignment
+        # list is created the following will be used to create
+        # the UCF,TCL,SDC,etc. files.
+        self.pattr = pattr
+
+        # @todo: need to define the common pin attribute in 
+        #    a technology agonostic, the tool-chain will convert
+        #    to the specific constraints.
+        #    drive
+        #    pullup
+
