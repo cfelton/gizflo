46072d8a996a5c14294f849c5c3e8ad090132b4e
Christopher Felton <chris.felton@gmail.com>
2014-10-04 08:56:52 -0500

    xula SDRAM definitions

--- gizflo/boards/xilinx/_xula.py
+++ gizflo/boards/xilinx/_xula.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from ..._fpga import _fpga
+from ...extintf._sdram import SDRAM
 
 class Xula(_fpga):
     vendor = 'xilinx'
@@ -34,6 +35,8 @@ class Xula(_fpga):
                             62,68,72,73,82,83,84,35,  # 8-15
                             34,33,32,21,20,19,13,12,  # 17-23
                             07,04,03,97,94,93,89,88)) # 24-31
+
+        
     }
 
 
@@ -57,4 +60,63 @@ class Xula2(_fpga):
                              'M2','M1','K3','J4','H1','H2',       #18-23
                              'F1','F2','E1','E2','C1','B1',       #24-29
                              'B2','A2',) )
+      }
+
+
+      default_extintf = {
+          #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # VGA:
+          'vga': None,  
+ 
+          #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # SDRAM: the Xula2 has a 256Mbit WINBond SDRAM,
+          # http://www.winbond.com/hq/enu/ProductAndSales/ProductLines/SpecialtyDRAM/SDRAM/W9825G6JH.htm
+          'sdram': SDRAM(
+              Port('addr', pins=('E4',  'E3',  'D3',  'C3',   # 0-3
+                                 'B12', 'A12', 'D12', 'E12',  # 4-7
+                                 'G16', 'G12', 'F4',  'G11',  # 8-11
+                                 'H13',)                      # 12
+                   ),
+              Port('data', pins=('P6',  'T6',  'T5',  'P5',   # 0-3
+                                 'R5',  'N5',  'P4',  'N4',   # 4-7
+                                 'P12', 'R12', 'T13', 'T14',  # 8-11
+                                 'R14', 'T15', 'T12', 'P11',) # 12-15
+               ),
+              Port('bs',   pins=('H3', 'G3',) ),
+              Port('cas',  pins=('L3',) ),
+              Port('ras',  pins=('L4',) ),
+              Port('ldqm', pins=('M4',) ),
+              Port('udqm', pins=('L13',) ),
+              Port('clk',  pins=('K12',) ),
+              Port('clkfb', pins=('K11',) ),
+              Port('cs', pins=('H4',) ),
+              Port('we', pins=('M3',) ),
+              Port('cke',  pins=('J12',))
+              
+              # timing information, all in ns
+              timing = dict(
+                  init = 200000.0,
+                  ras  = 45.0,
+                  rcd  = 20.0,
+                  ref  = 64000000.0,
+                  rfc  = 65.0,
+                  rp   = 20.0,
+                  xsr  = 75.0                  
+               ),
+              ddr = 0  # single data rate
+          ),
+
+          #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          #  SPI and MicroSD
+          'flash': _extintf(
+              Port('sclk', pins=(,)),
+              Port('sdi', pins=(,)),
+              Port('sdo', pins=(,)),
+              port('cs', pins=(,)),
+              )
+
+          'microsd' : None,
+
+          #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          # 
       }
--- gizflo/extintf/_sdram.py
+++ gizflo/extintf/_sdram.py
@@ -4,6 +4,8 @@ from _extintf import _extintf
 
 class SDRAM(_extintf):
     """
+    Define the interface to SDRAMs.  This includes single and 
+    double data rate SDRAMs.
     """
     
     # the timing atrribute captures all the time fields of the 
@@ -20,4 +22,53 @@ class SDRAM(_extintf):
         'xsr':  75.0,       # exit self-refresh time
     }
 
+    default_ports = {
+        'data': None,  # D0-D15, data inputs
+        'addr': None,  # A0-A12, address inputs
+
+        'dqs':  None,  # data strobe
+        'udqs': None,  # data strobe, upper byte
+        'ldqs': None,  # data strobe, lower byte
+
+        'dqm':, None,  # data mask
+        'udqm': None,  # upper data mask
+        'ldqm': None,  # lower data mask
+
+        'bs':   None,  # bank select  (bs == ba)
+        'ba':   None,  # bank address
+
+        'ras':  None,  # row refresh, normally active-low
+        'cas':  None,  # column refresh, normally active-low
+        'we':   None,  # write-enable, normally active-low
+        'cs':   None,  # chip-select, normally active-low
+        'cke':  None,  # clock enable, normally active-high
+        'clk':  None,
+        'clkp': None,  # ddr positive clock
+        'clkn': None,  # ddr negative clock
+    }
+
+    def __init__(self, *ports, **params):
+
+        # is this DDR SDRAM or SDR SDRAM (ddr=0)
+        if params.has_key('ddr'):
+            self.ddr = params['ddr']
+        else:
+            self.ddr = 0
+
+        # walk through the ports and update
+        self._ports = dict(self.default_ports)
+        for pp in ports:
+            self._ports[pp.name] = pp
+
+        # misc parameters
+        self._params = dict(params)
+
+        # update any of the timing information
+        self._timing = dict(self.default_timing)
+        for k,v in params.items():
+            if self._timing.has_key(k):
+                self._timing[k] = v
+        
+
+    
     
